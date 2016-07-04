8398933187641d9bbbbeb711e365bb34b2720e67
Robin Karlsson <s.r.karlsson@gmail.com>
2016-07-04 15:54:42 +0200

    added pop/push test cases

--- script/test.py
+++ script/test.py
@@ -41,5 +41,29 @@ class TestDisassembleRaw(unittest.TestCase):
             "ADC\teax, 0xefbeadde")
         disasm.stdout.close()
 
+    # PUSH 0x50+r - PUSH 0x57
+    def test_push(self):
+        reg64 = [ "rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi" ]
+        opcode = 50
+        for opcode in range (50, 57):
+            self.create_bin(str(opcode))
+            disasm = subprocess.Popen(["./rdisasm","-braw", "tmp.bin"], stdout=subprocess.PIPE)
+            inst = "push\t"+reg64[opcode-50]
+            self.assertEqual(str(disasm.stdout.read(), encoding="utf-8").rstrip(),
+                inst)
+            disasm.stdout.close()
+
+    # POP 0x58 - 0x5f
+    def test_pop(self):
+        reg64 = [ "rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi" ]
+        opcode = 50
+        for opcode in range (0x58, 0x5f):
+            self.create_bin(hex(opcode).replace("0x",""))
+            disasm = subprocess.Popen(["./rdisasm","-braw", "tmp.bin"], stdout=subprocess.PIPE)
+            inst = "pop\t"+reg64[opcode-0x58]
+            self.assertEqual(str(disasm.stdout.read(), encoding="utf-8").rstrip(),
+                inst)
+            disasm.stdout.close()
+
 if __name__ == "__main__":
     unittest.main()
